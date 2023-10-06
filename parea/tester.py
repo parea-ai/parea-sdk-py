b2b37b701c149d0b4b8d238de0b34466231b475c
Joschka Braun <joschka.braun@gmail.com>
2023-10-06 22:21:08 +0200

    feat: execute user function via cli in parallel with traces working

--- parea/tester.py
+++ parea/tester.py
@@ -1,4 +1,5 @@
 import concurrent
+import importlib
 import os
 import csv
 import argparse
@@ -6,15 +7,26 @@ import sys
 from importlib import machinery, util
 
 
-def load_from_path(path_to_module, attr_name):
-    module_name = os.path.basename(path_to_module)
-    loader = machinery.SourceFileLoader(module_name, path_to_module)
-    spec = util.spec_from_file_location(module_name, path_to_module, loader=loader)
-    module = util.module_from_spec(spec)
-    sys.modules[spec.name] = module
+def load_from_path(module_path, attr_name):
+    # Ensure the directory of user-provided script is in the system path
+    dir_name = os.path.dirname(module_path)
+    if dir_name not in sys.path:
+        sys.path.insert(0, dir_name)
+
+    module_name = os.path.basename(module_path)
+    # Add .py extension back in to allow import correctly
+    module_path_with_ext = f"{module_path}.py"
+
+    spec = importlib.util.spec_from_file_location(module_name, module_path_with_ext)
+    module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
+
+    if spec.name not in sys.modules:
+        sys.modules[spec.name] = module
+
     fn = getattr(module, attr_name)
     return fn
+
 
 
 def read_input_file(file_path):
@@ -34,7 +46,7 @@ if __name__ == "__main__":
 
     data_inputs = read_input_file(args.inputs)
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
+    with concurrent.futures.ProcessPoolExecutor() as executor:
         results = list(executor.map(fn, data_inputs))
 
     for i, result in enumerate(results):
