a5390f9190406c15c14f4f899fec62e945e724e3
Joschka Braun <joschka.braun@gmail.com>
2023-10-10 21:40:22 +0200

    chore: bump version

--- parea/helpers.py
+++ parea/helpers.py
@@ -13,4 +13,3 @@ def to_date_and_time_string(timestamp: float) -> str:
 
 def date_and_time_string_to_timestamp(date_and_time_string: str) -> float:
     return time.mktime(time.strptime(date_and_time_string, "%Y-%m-%d %H:%M:%S %Z"))
-
