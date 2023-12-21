ef2e979f42691654142cbf69085e11b553e13c62
Joel Alexander <a.joeljr@hotmail.com>
2023-12-20 23:17:44 -0500

    nit

--- parea/cookbook/langchain/trace_langchain_RAG_evals.py
+++ parea/cookbook/langchain/trace_langchain_RAG_evals.py
@@ -36,7 +36,6 @@ p = Parea(api_key=os.getenv("PAREA_API_KEY"))
 
 class DocumentRetriever:
     def __init__(self, url: str):
-        # Load in langsmith documentation as test
         api_loader = RecursiveUrlLoader(url)
         raw_documents = api_loader.load()
 
