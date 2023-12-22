1352bc38aecb66da2e2a0bf3e46fa4462991dd4a
Joel Alexander <a.joeljr@hotmail.com>
2023-12-22 13:43:38 -0500

    add url

--- parea/cookbook/langchain/trace_langchain_RAG_evals.py
+++ parea/cookbook/langchain/trace_langchain_RAG_evals.py
@@ -109,29 +109,29 @@ class DocumentationChain:
 
 # EXAMPLE EVALUATION TEST CASES
 eval_questions = [
-    # "What is the population of New York City as of 2020?",
+    "What is the population of New York City as of 2020?",
     "Which borough of New York City has the highest population? Only respond with the name of the borough.",
-    # "What is the economic significance of New York City?",
-    # "How did New York City get its name?",
-    # "What is the significance of the Statue of Liberty in New York City?",
+    "What is the economic significance of New York City?",
+    "How did New York City get its name?",
+    "What is the significance of the Statue of Liberty in New York City?",
 ]
 
 eval_answers = [
-    # "8,804,190",
+    "8,804,190",
     "Brooklyn",
-    # """New York City's economic significance is vast, as it serves as the global financial capital, housing Wall
-    # Street and major financial institutions. Its diverse economy spans technology, media, healthcare, education,
-    # and more, making it resilient to economic fluctuations. NYC is a hub for international business, attracting
-    # global companies, and boasts a large, skilled labor force. Its real estate market, tourism, cultural industries,
-    # and educational institutions further fuel its economic prowess. The city's transportation network and global
-    # influence amplify its impact on the world stage, solidifying its status as a vital economic player and cultural
-    # epicenter.""",
-    # """New York City got its name when it came under British control in 1664. King Charles II of England granted the
-    # lands to his brother, the Duke of York, who named the city New York in his own honor.""",
-    # """The Statue of Liberty in New York City holds great significance as a symbol of the United States and its
-    # ideals of liberty and peace. It greeted millions of immigrants who arrived in the U.S. by ship in the late 19th
-    # and early 20th centuries, representing hope and freedom for those seeking a better life. It has since become an
-    # iconic landmark and a global symbol of cultural diversity and freedom.""",
+    """New York City's economic significance is vast, as it serves as the global financial capital, housing Wall
+    Street and major financial institutions. Its diverse economy spans technology, media, healthcare, education,
+    and more, making it resilient to economic fluctuations. NYC is a hub for international business, attracting
+    global companies, and boasts a large, skilled labor force. Its real estate market, tourism, cultural industries,
+    and educational institutions further fuel its economic prowess. The city's transportation network and global
+    influence amplify its impact on the world stage, solidifying its status as a vital economic player and cultural
+    epicenter.""",
+    """New York City got its name when it came under British control in 1664. King Charles II of England granted the
+    lands to his brother, the Duke of York, who named the city New York in his own honor.""",
+    """The Statue of Liberty in New York City holds great significance as a symbol of the United States and its
+    ideals of liberty and peace. It greeted millions of immigrants who arrived in the U.S. by ship in the late 19th
+    and early 20th centuries, representing hope and freedom for those seeking a better life. It has since become an
+    iconic landmark and a global symbol of cultural diversity and freedom.""",
 ]
 
 # set up evaluation functions we want to test, provide the name of the relevant fields needed for each eval function
