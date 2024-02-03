354c00c873f983634556c5dfa61a7711c4e618dc
Joel Alexander <a.joeljr@hotmail.com>
2024-02-02 19:04:23 -0500

    fix(azure): calc azure model costs

--- parea/wrapper/utils.py
+++ parea/wrapper/utils.py
@@ -1,7 +1,8 @@
+from typing import Callable, Optional, Union
+
 import json
 import sys
 from functools import lru_cache, wraps
-from typing import Callable, Optional, Union
 
 import tiktoken
 from openai import __version__ as openai_version
@@ -50,19 +51,19 @@ def _num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613", is_azure: bo
     if (
         model
         in {
-        "gpt-3.5-turbo",
-        "gpt-3.5-turbo-1106",
-        "gpt-3.5-turbo-0125",
-        "gpt-3.5-turbo-0613",
-        "gpt-3.5-turbo-16k-0613",
-        "gpt-4-0314",
-        "gpt-4-32k-0314",
-        "gpt-4-0613",
-        "gpt-4-32k-0613",
-        "gpt-4-turbo-preview",
-        "gpt-4-1106-preview",
-        "gpt-4-0125-preview",
-    }
+            "gpt-3.5-turbo",
+            "gpt-3.5-turbo-1106",
+            "gpt-3.5-turbo-0125",
+            "gpt-3.5-turbo-0613",
+            "gpt-3.5-turbo-16k-0613",
+            "gpt-4-0314",
+            "gpt-4-32k-0314",
+            "gpt-4-0613",
+            "gpt-4-32k-0613",
+            "gpt-4-turbo-preview",
+            "gpt-4-1106-preview",
+            "gpt-4-0125-preview",
+        }
         or is_azure
     ):
         tokens_per_message = 3
@@ -157,9 +158,7 @@ def _calculate_input_tokens(
 ) -> int:
     is_azure = model.startswith("azure_") or model in AZURE_MODEL_INFO
     num_function_tokens = _num_tokens_from_functions(functions, function_call, model)
-    num_input_tokens = _num_tokens_from_string(json.dumps(messages),
-                                               model) if model == "gpt-4-vision-preview" else _num_tokens_from_messages(
-        messages, model, is_azure)
+    num_input_tokens = _num_tokens_from_string(json.dumps(messages), model) if model == "gpt-4-vision-preview" else _num_tokens_from_messages(messages, model, is_azure)
     return num_input_tokens + num_function_tokens
 
 
@@ -193,8 +192,7 @@ def _kwargs_to_llm_configuration(kwargs, model=None):
         provider="openai",
         messages=kwargs.get("messages", None),
         functions=functions,
-        function_call=kwargs.get("function_call", function_call_default) or kwargs.get("tool_choice",
-                                                                                       function_call_default),
+        function_call=kwargs.get("function_call", function_call_default) or kwargs.get("tool_choice", function_call_default),
         model_params=ModelParams(
             temp=kwargs.get("temperature", 1.0),
             max_length=kwargs.get("max_tokens", None),
@@ -279,8 +277,7 @@ def convert_openai_raw_stream_to_log(content: list, tools: dict, data: dict, tra
 
 
 def convert_openai_raw_to_log(r: dict, data: dict):
-    log_in_thread(_process_response,
-                  {"response": ChatCompletion(**r), "model_inputs": data, "trace_id": get_current_trace_id()})
+    log_in_thread(_process_response, {"response": ChatCompletion(**r), "model_inputs": data, "trace_id": get_current_trace_id()})
 
 
 CHUNK_DONE_SENTINEL = "data: [DONE]"
