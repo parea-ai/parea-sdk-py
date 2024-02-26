55eb59cfc0633d834ec21ea85546d07f2b985e1c
Joschka Braun <joschka.braun@gmail.com>
2024-02-26 15:06:59 -0500

    feat: add context has answer eval metric

--- parea/evals/rag/__init__.py
+++ parea/evals/rag/__init__.py
@@ -5,3 +5,4 @@ from .context_query_relevancy import context_query_relevancy_factory
 from .context_ranking_listwise import context_ranking_listwise_factory
 from .context_ranking_pointwise import context_ranking_pointwise_factory
 from .percent_target_supported_by_context import percent_target_supported_by_context_factory
+from .context_has_answer import context_has_answer_factory
