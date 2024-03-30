57a20653ba333ee1c16618a9580a30fd0bad7147
Joschka Braun <joschka.braun@gmail.com>
2024-03-30 17:18:22 -0400

    fix: update langchain samples

--- parea/cookbook/tracing_with_deployed_prompt.py
+++ parea/cookbook/tracing_with_deployed_prompt.py
@@ -16,7 +16,7 @@ p = Parea(api_key=os.getenv("PAREA_API_KEY"))
 def deployed_argument_generator(query: str, additional_description: str = "") -> str:
     return p.completion(
         Completion(
-            deployment_id="p-XOh3kp8B0nIE82WgioPnr",
+            deployment_id="p-RG8d9rfJc_0cctwfpb_n6",
             llm_inputs={
                 "additional_description": additional_description,
                 "date": f"{datetime.now()}",
@@ -29,7 +29,7 @@ def deployed_argument_generator(query: str, additional_description: str = "") ->
 def deployed_critic(argument: str) -> str:
     return p.completion(
         Completion(
-            deployment_id="p-PSOwRyIPaQRq4xQW3MbpV",
+            deployment_id="p-fXgZytT3dJjXD_71TDR4s",
             llm_inputs={"argument": argument},
         )
     ).content
@@ -38,7 +38,7 @@ def deployed_critic(argument: str) -> str:
 def deployed_refiner(query: str, additional_description: str, current_arg: str, criticism: str) -> str:
     return p.completion(
         Completion(
-            deployment_id="p-bJ3-UKh9-ixapZafaRBsj",
+            deployment_id="p--G2s9okMTvBEh3d8YqLY2",
             llm_inputs={
                 "additional_description": additional_description,
                 "date": f"{datetime.now()}",
@@ -53,7 +53,7 @@ def deployed_refiner(query: str, additional_description: str, current_arg: str, 
 def deployed_refiner2(query: str, additional_description: str, current_arg: str, criticism: str) -> CompletionResponse:
     return p.completion(
         Completion(
-            deployment_id="p-bJ3-UKh9-ixapZafaRBsj",
+            deployment_id="p--G2s9okMTvBEh3d8YqLY2",
             llm_inputs={
                 "additional_description": additional_description,
                 "date": f"{datetime.now()}",
@@ -65,11 +65,11 @@ def deployed_refiner2(query: str, additional_description: str, current_arg: str,
     )
 
 
-@trace
-def deployed_argument_chain(query: str, additional_description: str = "") -> str:
-    argument = deployed_argument_generator(query, additional_description)
-    criticism = deployed_critic(argument)
-    return deployed_refiner(query, additional_description, argument, criticism)
+# @trace
+# def deployed_argument_chain(query: str, additional_description: str = "") -> str:
+#     argument = deployed_argument_generator(query, additional_description)
+#     criticism = deployed_critic(argument)
+#     return deployed_refiner(query, additional_description, argument, criticism)
 
 
 @trace(
@@ -84,11 +84,11 @@ def deployed_argument_chain_tags_metadata(query: str, additional_description: st
 
 
 if __name__ == "__main__":
-    result1 = deployed_argument_chain(
-        "Whether coffee is good for you.",
-        additional_description="Provide a concise, few sentence argument on why coffee is good for you.",
-    )
-    print(result1)
+    # result1 = deployed_argument_chain(
+    #     "Whether coffee is good for you.",
+    #     additional_description="Provide a concise, few sentence argument on why coffee is good for you.",
+    # )
+    # print(result1)
 
     result2, trace_id = deployed_argument_chain_tags_metadata(
         "Whether coffee is good for you.",
