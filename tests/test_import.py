b7e498f1aece589305e0b98ab1f2519da7b5bdeb
Joschka Braun <joschka.braun@gmail.com>
2024-04-24 08:38:00 -0400

    style

--- tests/test_import.py
+++ tests/test_import.py
@@ -6,8 +6,8 @@ import pytest
 
 def test_imports():
     try:
-        package = importlib.import_module('parea')
+        package = importlib.import_module("parea")
         for _, module_name, _ in pkgutil.iter_modules(package.__path__):
-            importlib.import_module(f'parea.{module_name}')
+            importlib.import_module(f"parea.{module_name}")
     except ImportError:
         pytest.fail("Import failed", pytrace=False)
