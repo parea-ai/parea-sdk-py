6e7efe70e3f7788000070966ddf9964a2fe98374
Joschka Braun <joschka.braun@gmail.com>
2024-04-24 08:36:34 -0400

    test: add import test

---
+++ tests/test_import.py
@@ -0,0 +1,13 @@
+import importlib
+import pkgutil
+
+import pytest
+
+
+def test_imports():
+    try:
+        package = importlib.import_module('parea')
+        for _, module_name, _ in pkgutil.iter_modules(package.__path__):
+            importlib.import_module(f'parea.{module_name}')
+    except ImportError:
+        pytest.fail("Import failed", pytrace=False)
