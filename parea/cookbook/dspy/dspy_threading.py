4e2ce3810cd49dd931176d71a16a0dcd9cc37db6
Joschka Braun <joschka.braun@gmail.com>
2024-05-16 14:11:30 -0400

    docs: add cookbooks for threading & multi-processing with trace decorator

---
+++ parea/cookbook/dspy/dspy_threading.py
@@ -0,0 +1,45 @@
+from concurrent.futures import ThreadPoolExecutor
+import contextvars
+import os
+
+import dspy
+from dotenv import load_dotenv
+
+from parea import Parea
+
+load_dotenv()
+
+p = Parea(api_key=os.getenv("PAREA_API_KEY"))
+p.trace_dspy()
+
+gpt3_turbo = dspy.OpenAI(model="gpt-3.5-turbo-1106", max_tokens=300)
+dspy.configure(lm=gpt3_turbo)
+
+
+class QASignature(dspy.Signature):
+    question = dspy.InputField()
+    answer = dspy.OutputField()
+
+
+class EnsembleQA(dspy.Module):
+    def __init__(self):
+        super().__init__()
+        self.step1 = dspy.ChainOfThought(QASignature)
+        self.step2 = dspy.ChainOfThought(QASignature)
+
+    def forward(self, question):
+        with ThreadPoolExecutor(max_workers=2) as executor:
+            context1 = contextvars.copy_context()
+            future1 = executor.submit(context1.run, self.step1, question=question)
+            context2 = contextvars.copy_context()
+            future2 = executor.submit(context2.run, self.step2, question=question + "?")
+
+        answer1 = future1.result()
+        answer2 = future2.result()
+
+        return dspy.Prediction(answer=f"{answer1}\n\n{answer2}")
+
+
+qa = EnsembleQA()
+response = qa("Who are you?")
+print(response.answer)
---
+++ parea/cookbook/tracing_with_threading.py
@@ -0,0 +1,33 @@
+from concurrent.futures import ThreadPoolExecutor
+import contextvars
+import os
+from typing import List
+
+from dotenv import load_dotenv
+
+from parea import Parea, trace
+
+
+load_dotenv()
+
+p = Parea(api_key=os.getenv("PAREA_API_KEY"))
+
+
+@trace
+def llm_call(question):
+    return f"I can't answer that question: {question}"
+
+
+@trace
+def multiple_llm_calls(question, n_calls: int = 2) -> List[str]:
+    answers = []
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        for _ in range(n_calls):
+            context = contextvars.copy_context()
+            future = executor.submit(context.run, llm_call, question)
+            answers.append(future.result())
+    return answers
+
+
+response = multiple_llm_calls("Who are you?")
+print(response)
