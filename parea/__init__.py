c3ef132347ea988221d5403acebfe6cfb7778871
Joel Alexander <a.joeljr@hotmail.com>
2024-06-04 16:58:48 -0400

    helper for lamdas

--- parea/__init__.py
+++ parea/__init__.py
@@ -18,7 +18,7 @@ from parea.experiment.dvc import parea_dvc_initialized
 from parea.experiment.experiment import Experiment
 from parea.helpers import gen_trace_id, write_trace_logs_to_csv
 from parea.parea_logger import parea_logger
-from parea.utils.trace_utils import get_current_trace_id, get_root_trace_id, trace, trace_insert
+from parea.utils.trace_utils import clear_trace_context, get_current_trace_id, get_root_trace_id, trace, trace_insert
 from parea.wrapper.openai_raw_api_tracer import aprocess_stream_and_yield, process_stream_and_yield
 from parea.wrapper.utils import convert_openai_raw_to_log
 
