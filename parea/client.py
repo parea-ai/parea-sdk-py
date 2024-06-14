84b34d115a7a1162b1d8b169168086b4033ad96f
Joschka Braun <joschka.braun@gmail.com>
2024-06-13 21:18:07 -0400

    feat: introduce flag for stop on error

--- parea/client.py
+++ parea/client.py
@@ -352,6 +352,7 @@ class Parea:
         metadata: Optional[Dict[str, str]] = None,
         dataset_level_evals: Optional[List[Callable]] = None,
         n_workers: int = 10,
+        stop_on_error: bool = False,
     ):
         """
         :param data: If your dataset is defined locally it should be an iterable of k/v
@@ -363,6 +364,7 @@ class Parea:
         :param metadata: Optional metadata to attach to the experiment.
         :param dataset_level_evals: Optional list of functions to run on the dataset level. Each function should accept a list of EvaluatedLog objects and return a float or an EvaluationResult object
         :param n_workers: The number of workers to use for running the experiment.
+        :param stop_on_error: If True, the experiment will stop on the first exception. If False, the experiment will continue running the remaining samples.
         """
         from parea import Experiment
 
@@ -375,6 +377,7 @@ class Parea:
             metadata=metadata,
             dataset_level_evals=dataset_level_evals,
             n_workers=n_workers,
+            stop_on_error=stop_on_error,
         )
 
     def _update_data_and_trace(self, data: Completion) -> Completion:
