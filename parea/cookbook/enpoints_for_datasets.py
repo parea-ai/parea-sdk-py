37179962c0e68ab2b5e55048c4e3b878dee31478
Joel Alexander <a.joeljr@hotmail.com>
2024-07-02 18:17:54 -0400

    add edit test case endpoint

--- parea/cookbook/enpoints_for_datasets.py
+++ parea/cookbook/enpoints_for_datasets.py
@@ -12,15 +12,15 @@ p = Parea(api_key=os.getenv("PAREA_API_KEY"))
 
 data = [{"problem": "1+2", "target": 3, "tags": ["easy"]}, {"problem": "Solve the differential equation dy/dx = 3y.", "target": "y = c * e^(3x)", "tags": ["hard"]}]
 
-# this will create a new dataset on Parea named "Math problems".
+# this will create a new dataset on Parea named "math_problems_v4".
 # The dataset will have one column named "problem", and two columns using the reserved names "target" and "tags".
 # when using this dataset the expected prompt template should have a placeholder for the varible problem.
-# p.create_test_collection(data, name="math_problems_v5")
+p.create_test_collection(data, name="math_problems_v4")
 
 new_data = [{"problem": "Evaluate the integral ∫x^2 dx from 0 to 3.", "target": 9, "tags": ["hard"]}]
-# this will add the new test cases to the existing "Math problems" dataset.
+# this will add the new test cases to the existing "math_problems_v4" dataset.
 # New test cases must have the same columns as the existing dataset.
-# p.add_test_cases(new_data, name="math_problems_v5")
+p.add_test_cases(new_data, name="math_problems_v4")
 # Or if you can use the dataset ID instead of the name
 # p.add_test_cases(new_data, dataset_id=121)
 
