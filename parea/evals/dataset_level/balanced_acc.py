2fe2de28a04623fbc092e0ec147788a8e99afb0e
Joschka Braun <joschka.braun@gmail.com>
2024-07-10 13:42:47 +0200

    fix: no recalls in balanced accuracy

--- parea/evals/dataset_level/balanced_acc.py
+++ parea/evals/dataset_level/balanced_acc.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Union
 
 from collections import defaultdict
 
@@ -6,7 +6,7 @@ from parea.schemas import EvaluatedLog, EvaluationResult
 
 
 def balanced_acc_factory(score_name: str):
-    def balanced_acc(logs: List[EvaluatedLog]) -> EvaluationResult:
+    def balanced_acc(logs: List[EvaluatedLog]) -> Union[EvaluationResult, None]:
         correct = defaultdict(int)
         total = defaultdict(int)
         for log in logs:
@@ -14,6 +14,9 @@ def balanced_acc_factory(score_name: str):
                 correct[log.target] += int(eval_result.score)
                 total[log.target] += 1
         recalls = [correct[key] / total[key] for key in correct]
+
+        if len(recalls) == 0:
+            return None
 
         return EvaluationResult(name=f"balanced_acc_{score_name}", score=sum(recalls) / len(recalls))
 
