52179bd7ed8a80f8778c5a7f0a04094d46c88ad4
Joel Alexander <a.joeljr@hotmail.com>
2024-08-07 08:47:08 -0400

    fix costs

--- parea/constants.py
+++ parea/constants.py
@@ -1,6 +1,5 @@
-from typing import Dict, Union
-
 import os
+from typing import Dict, Union
 
 from dotenv import load_dotenv
 
@@ -147,8 +146,8 @@ OPENAI_MODEL_INFO: Dict[str, Dict[str, Union[float, int, Dict[str, int]]]] = {
         "token_limit": {"max_completion_tokens": 4096, "max_prompt_tokens": 128000},
     },
     "gpt-4o-2024-08-06": {
-        "prompt": 5.0,
-        "completion": 15.0,
+        "prompt": 2.5,
+        "completion": 10.0,
         "token_limit": {"max_completion_tokens": 4096, "max_prompt_tokens": 128000},
     },
     "gpt-4o-mini": {
